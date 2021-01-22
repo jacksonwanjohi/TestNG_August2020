8b519a2587780bb105d131fb3c922bf3f808d81b
jacksonwanjohi <75943103+jacksonwanjohi@users.noreply.github.com>
2021-01-21 20:28:53 -0500

    2nd Commit

---
+++ src/main/java/variousConcepts/TestGit.java
@@ -0,0 +1,17 @@
+package variousConcepts;
+
+public class TestGit {
+	
+	public static void main() {
+		printMe();
+		
+	
+	}
+
+	public static void printMe() {
+		
+		System.out.println("Test Git");
+
+	}
+	
+}
